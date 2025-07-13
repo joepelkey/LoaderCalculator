b84b372fc690971a5d86ac661fb3c9c28db9469d
joepelkey <133468551+joepelkey@users.noreply.github.com>
2025-07-12 21:10:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
-st.set_page_config(page_title="Load PPH Tracker")
+st.set_page_config(page_title="Production Tracker")
 
-st.title("Load PPH Tracker")
+st.title("Production Tracker")
 
-packages = st.number_input("Packages Loaded", min_value=0)
-hours = st.number_input("Hours Loaded", min_value=0.0, format="%.2f")
-target = st.number_input("Target PPH", min_value=1, value=350)
+packages = st.number_input("Packages Processed", min_value=0)
+hours = st.number_input("Hours", min_value=0.0, format="%.2f")
+target = st.number_input("Plan PPH", min_value=1, value=350)
 
 if hours > 0:
     actual_pph = packages / hours
